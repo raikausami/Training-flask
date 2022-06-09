e12840e93639b26073c73239ad09eec4492ee28f
unknown <you@example.com>
2022-06-09 23:32:09 +0900

    first commit

--- Training-flask/Training_flask/views.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime
-from flask import render_template
-from Training_flask import app
-
-@app.route('/')
-@app.route('/home')
-
-def home():
-    return "Hello world"
--- Training-flask/runserver.py
+++ Training-flask/runserver.py
@@ -2,14 +2,15 @@
 This script runs the Training_flask application using a development server.
 """
 import os
-from Training_flask import app
+
+from flask import Flask
+from HelloFlask import app
 
 if __name__ == '__main__':
+    import os
     HOST = os.environ.get('SERVER_HOST', 'localhost')
-
     try:
         PORT = int(os.environ.get('SERVER_PORT', '5555'))
     except ValueError:
         PORT = 5555
-
     app.run(HOST, PORT)
